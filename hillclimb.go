250d756da61cdf91e0e061bf10267ba622db0234
Shreyas Sriram <shreyassriram99@gmail.com>
2021-09-06 01:03:48 -0400

    Refactor

--- hillclimb.go
+++ hillclimb.go
@@ -1,5 +1,5 @@
 // enigma "LOTSOFPEOPLEWOR" --rotors "Gamma VI IV III" --rings="1 1 1 16" --position "D A B Q" --plugboard "MS KU FY AG BN PQ HJ DI ER LW" --reflector "C-thin"
-// Use `github.com/becgabri/enigma`
+// Program uses `github.com/emedvedev/enigma` as an external API to test enigma configurations
 
 package main
 
@@ -9,25 +9,29 @@ import (
 	"os"
 )
 
-type Enigma struct {
-	Reflector string
-	Rings     []int
-	Positions []string
-	Rotors    []string
+// enigma stores the enigma configuration
+type enigma struct {
+	reflector string
+	rings     []int
+	positions []string
+	rotors    []string
+	plugboard string
 }
 
-var defaultConfig = Enigma{
-	Reflector: "C-thin",
-	Rings:     []int{1, 1, 1, 16},
-	Positions: []string{"", "", "B", "Q"},
-	Rotors:    []string{"", "", "IV", "III"},
+var currentConfig = enigma{
+	reflector: "C-thin",
+	rings:     []int{1, 1, 1, 16},
+	positions: []string{"_", "_", "B", "Q"},
+	rotors:    []string{"_", "_", "IV", "III"},
+	plugboard: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 }
 
-var bestConfig = Enigma{
-	Reflector: "C-thin",
-	Rings:     []int{1, 1, 1, 16},
-	Positions: []string{"", "", "B", "Q"},
-	Rotors:    []string{"", "", "IV", "III"},
+var bestConfig = enigma{
+	reflector: "C-thin",
+	rings:     []int{1, 1, 1, 16},
+	positions: []string{"", "", "B", "Q"},
+	rotors:    []string{"", "", "IV", "III"},
+	plugboard: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 }
 
 const (
@@ -42,20 +46,14 @@ var (
 )
 
 func main() {
-	fmt.Printf("\n --- Enigma cryptanalysis --- \n")
+	fmt.Printf("\n --- enigma cryptanalysis --- \n")
 
 	if len(os.Args) != 2 {
 		fmt.Printf("\n [-] Cipher text file not provided, exiting\n")
 		os.Exit(0)
 	}
 
-	cipherBytes, err := readFile(os.Args[1])
-	if err != nil {
-		fmt.Printf("\n [-] Error reading cipher text file: %v", err)
-		os.Exit(0)
-	}
-
-	cipherText := string(cipherBytes)
+	cipherText := readFile(os.Args[1])
 
 	fmt.Printf("\n [+] Read cipher text :\n %v", cipherText)
 
@@ -69,37 +67,38 @@ func main() {
 				continue
 			}
 
-			defaultConfig.Rotors[0] = rotorPositionOne
-			defaultConfig.Rotors[1] = rotorPositionTwo
+			currentConfig.rotors[0] = rotorPositionOne
+			currentConfig.rotors[1] = rotorPositionTwo
+
+			fmt.Printf("\n\n [+] Trying enigma configuration:")
+			printConfig(currentConfig)
 
 			for i := 0; i < 26; i++ { // starting position of rotor at 1st place
 				for j := 0; j < 26; j++ { // starting position of rotor at 2nd place
-					defaultConfig.Positions[0] = string(rune(i + 65))
-					defaultConfig.Positions[1] = string(rune(j + 65))
-
-					printConfig(defaultConfig)
+					currentConfig.positions[0] = string(rune(i + 65))
+					currentConfig.positions[1] = string(rune(j + 65))
 
 					currentPlugboard := getBestPlugboard(cipherText)
+					currentConfig.plugboard = currentPlugboard
 
-					plainText := runEnigma(cipherText, currentPlugboard)
+					plainText := runEnigma(cipherText, currentConfig)
 
 					currentTrigramScore := calculateTrigram(plainText)
 
-					fmt.Printf("\n Received Trigram score: %v", currentTrigramScore)
-					fmt.Printf("\n\n ----------------------")
+					// fmt.Printf("\n Received Trigram score: %v", currentTrigramScore)
 
 					if currentTrigramScore > bestTrigramScore {
 						bestTrigramScore = currentTrigramScore
-						copyStruct(&bestConfig, &defaultConfig)
+						copyStruct(&bestConfig, &currentConfig)
 					}
 				}
 			}
 		}
 	}
 
-	// res := formatPlugboard("UGCDEFBHIJKLMNOPQRSTAVWXYZ")
-	// fmt.Printf("\n\n\n Plugboard: %+v", res)
-
-	fmt.Printf("\n\n\n Best IOC score: %v", bestTrigramScore)
+	// fmt.Printf("\n\n\n Best IOC score: %v", bestTrigramScore)
 	printConfig(bestConfig)
+
+	plainText := runEnigma(cipherText, bestConfig)
+	fmt.Println(plainText)
 }
--- utils.go
+++ utils.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/emedvedev/enigma"
+	enigma_api "github.com/emedvedev/enigma"
 )
 
 var trigrams = make(map[string]float64)
@@ -17,209 +17,10 @@ const (
 	defaultPlugboard string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 )
 
-// readFile read a file and returns the contents as bytes
-func readFile(filename string) ([]byte, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Printf("\nError opening file: %v", filename)
-		return nil, fmt.Errorf("error opening file")
-	}
-
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Printf("\nError reading file: %v", filename)
-		return nil, fmt.Errorf("error reading file")
-	}
-
-	return fileBytes, nil
-}
-
-// calculateIOC calculates the `Index of Coincidence` for a given text
-func calculateIOC(text string) float64 {
-	ioc := float64(0)
-
-	for ascii := 65; ascii < 91; ascii++ {
-		fi := strings.Count(text, string(rune(ascii)))
-		ioc += float64(fi * (fi - 1))
-	}
-
-	n := float64(len(text))
-	ioc = float64(ioc / (n * (n - 1)))
-
-	return ioc
-}
-
-// formatPlugboard converts to raw plugboard string into the format
-// expected by `github.com/emedvedev/enigma` (string array)
-func formatPlugboard(rawPlugboard string) []string {
-	formattedPlugboard := make([]string, 0)
-
-	for i, letter := range rawPlugboard {
-		if string(rune(i+65)) < string(letter) {
-			formattedPlugboard = append(formattedPlugboard, string(rune(i+65))+string(letter))
-		}
-	}
-
-	return formattedPlugboard
-}
-
-func swap(a, b rune, plugboard string) string {
-	fmt.Printf("\n Swap %v and %v", string(a), string(b))
-
-	for i, letter := range plugboard {
-		if letter == a {
-			plugboard = plugboard[0:i] + string(b) + plugboard[i+1:]
-		} else if letter == b {
-			plugboard = plugboard[0:i] + string(a) + plugboard[i+1:]
-		}
-	}
-
-	return plugboard
-}
-
-func getBestPlugboard(cipherText string) string {
-	bestPlugboard := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-	plainText := runEnigma(cipherText, bestPlugboard)
-	bestIOC := calculateIOC(plainText)
-
-	for i := 0; i < 26; i++ {
-
-		currentIOC := bestIOC
-		currentBestPlugboard := bestPlugboard
-
-		for j := i + 1; j < 26; j++ {
-			var plugboards []string
-
-			fmt.Printf("\nCurrent plugboard: %v", bestPlugboard)
-
-			if defaultPlugboard[j] == bestPlugboard[j] {
-				plugboards = append(plugboards, swap(rune(defaultPlugboard[i]), rune(bestPlugboard[j]), bestPlugboard))
-				fmt.Printf("\nPlugboard: %v", plugboards)
-			} else {
-				revertedPlugboard := swap(rune(defaultPlugboard[j]), rune(bestPlugboard[j]), bestPlugboard)
-				fmt.Printf("\nSwapped plugboard: %v", revertedPlugboard)
-
-				revertedPlugboard = swap(rune(defaultPlugboard[i]), rune(bestPlugboard[i]), revertedPlugboard)
-				fmt.Printf("\nSwapped plugboard: %v", revertedPlugboard)
-
-				plugboards = append(plugboards, swap(rune(defaultPlugboard[i]), rune(bestPlugboard[i]), revertedPlugboard))
-				fmt.Printf("\nPlugboard: %v", plugboards)
-
-				plugboards = append(plugboards, swap(rune(defaultPlugboard[i]), rune(bestPlugboard[j]), revertedPlugboard))
-				fmt.Printf("\nPlugboards: %v", plugboards)
-			}
-
-			for _, plugboard := range plugboards {
-				plainText := runEnigma(cipherText, plugboard)
-				localIOC := calculateIOC(plainText)
-
-				fmt.Printf("\nlocalIOC: %v", localIOC)
-
-				if localIOC > currentIOC {
-					currentIOC = localIOC
-					currentBestPlugboard = plugboard
-				}
-			}
-		}
-
-		fmt.Printf("\ncurrentIOC: %v", currentIOC)
-		fmt.Printf("\nbestIOC: %v", bestIOC)
-
-		if currentIOC > bestIOC {
-			bestIOC = currentIOC
-			bestPlugboard = currentBestPlugboard
-		}
-	}
-
-	fmt.Printf("\nbestIOC: %v", bestIOC)
-	fmt.Printf("\nbestPlugboard: %v", bestPlugboard)
-
-	return bestPlugboard
-}
-
-// runEnigma configures and run the Enigma with the given settings
-// and returns the output
-func runEnigma(input, plugboard string) string {
-	config := make([]enigma.RotorConfig, len(defaultConfig.Rotors))
-	for index, rotor := range defaultConfig.Rotors {
-		ring := defaultConfig.Rings[index]
-		start := defaultConfig.Positions[index][0]
-		config[index] = enigma.RotorConfig{ID: rotor, Start: start, Ring: ring}
-	}
-
-	formattedPlugboard := formatPlugboard(plugboard)
-	fmt.Printf("\nRawplugboard: %v", defaultPlugboard)
-	fmt.Printf("\nRawplugboard: %v", plugboard)
-	fmt.Printf("\nFormatted plugboard: %v", formattedPlugboard)
-
-	e := enigma.NewEnigma(config, defaultConfig.Reflector, formattedPlugboard)
-	output := e.EncodeString(string(input))
-
-	return output
-}
-
-// func getScore(cipherText string, plugboard string) float64 {
-// 	// config := make([]enigma.RotorConfig, len(defaultConfig.Rotors))
-// 	// for index, rotor := range defaultConfig.Rotors {
-// 	// 	ring := defaultConfig.Rings[index]
-// 	// 	start := defaultConfig.Positions[index][0]
-// 	// 	config[index] = enigma.RotorConfig{ID: rotor, Start: start, Ring: ring}
-// 	// }
-
-// 	// formattedPlugboard := formatPlugboard(plugboard)
-
-// 	// // plugboards := strings.Split("MS KU FY AG BN PQ HJ DI ER LW", " ") // TODO : has to be permutated
-// 	// e := enigma.NewEnigma(config, defaultConfig.Reflector, formattedPlugboard)
-// 	// plainText := e.EncodeString(string(cipherText))
-
-// 	score := calculateIOC(plainText)
-
-// 	return score
-// }
-
-// printConfig pretty prints the configurtation of the Enigma
-func printConfig(config Enigma) {
-	fmt.Printf("\n\n Enigma configuration:")
-
-	fmt.Printf("\n\n\t Rotor configuration:\n\t\t")
-	for _, rotor := range config.Rotors {
-		fmt.Printf("%v ", rotor)
-	}
-
-	fmt.Printf("\n\n\t Rotor position configuration:\n\t\t")
-	for _, position := range config.Positions {
-		fmt.Printf("%v ", position)
-	}
-
-	fmt.Printf("\n\n\n")
-}
-
-// copyStruct makes a deep copy of `Enigma` struct into another
-// struct
-func copyStruct(dst *Enigma, src *Enigma) {
-	dst.Reflector = src.Reflector
-
-	dst.Rings = make([]int, len(src.Rings))
-	copy(dst.Rings, src.Rings)
-
-	dst.Positions = make([]string, len(src.Positions))
-	copy(dst.Positions, src.Positions)
-
-	dst.Rotors = make([]string, len(src.Rotors))
-	copy(dst.Rotors, src.Rotors)
-}
-
 // initializeTrigrams read and initializes the trigram scores from a file
 // into a map structure
 func initializeTrigrams(filename string) {
-	fileBytes, err := readFile(filename)
-	if err != nil {
-		if err != nil {
-			fmt.Printf("\nError reading file: %v", filename)
-			os.Exit(0)
-		}
-	}
+	fileBytes := readFile(filename)
 
 	file := string(fileBytes)
 	fileLines := strings.Split(file, "\n")
@@ -244,6 +45,179 @@ func initializeTrigrams(filename string) {
 	}
 }
 
+// readFile read a file and returns the contents as string
+func readFile(filename string) string {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("\nError opening file: %v", filename)
+		os.Exit(0)
+	}
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Printf("\nError reading file: %v", filename)
+		os.Exit(0)
+	}
+
+	return string(fileBytes)
+}
+
+// getBestPlugboard returns the best plugboard configuration based on
+// the `Index of Coincidence`
+func getBestPlugboard(cipherText string) string {
+	bestPlugboard := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	plainText := runEnigmaWithPlugboard(cipherText, bestPlugboard)
+	bestIOC := calculateIOC(plainText)
+
+	for i := 0; i < 26; i++ {
+
+		currentIOC := bestIOC
+		currentBestPlugboard := bestPlugboard
+
+		for j := i + 1; j < 26; j++ {
+			var plugboards []string
+
+			// fmt.Printf("\nCurrent plugboard: %v", bestPlugboard)
+
+			if defaultPlugboard[j] == bestPlugboard[j] {
+				plugboards = append(plugboards, swap(rune(defaultPlugboard[i]), rune(bestPlugboard[j]), bestPlugboard))
+				// fmt.Printf("\nPlugboard: %v", plugboards)
+			} else {
+				revertedPlugboard := swap(rune(defaultPlugboard[j]), rune(bestPlugboard[j]), bestPlugboard)
+				// fmt.Printf("\nSwapped plugboard: %v", revertedPlugboard)
+
+				revertedPlugboard = swap(rune(defaultPlugboard[i]), rune(bestPlugboard[i]), revertedPlugboard)
+				// fmt.Printf("\nSwapped plugboard: %v", revertedPlugboard)
+
+				plugboards = append(plugboards, swap(rune(defaultPlugboard[i]), rune(bestPlugboard[i]), revertedPlugboard))
+				// fmt.Printf("\nPlugboard: %v", plugboards)
+
+				plugboards = append(plugboards, swap(rune(defaultPlugboard[i]), rune(bestPlugboard[j]), revertedPlugboard))
+				// fmt.Printf("\nPlugboards: %v", plugboards)
+			}
+
+			for _, plugboard := range plugboards {
+				plainText := runEnigmaWithPlugboard(cipherText, plugboard)
+				localIOC := calculateIOC(plainText)
+
+				// fmt.Printf("\nlocalIOC: %v", localIOC)
+
+				if localIOC > currentIOC {
+					currentIOC = localIOC
+					currentBestPlugboard = plugboard
+				}
+			}
+		}
+
+		// fmt.Printf("\ncurrentIOC: %v", currentIOC)
+		// fmt.Printf("\nbestIOC: %v", bestIOC)
+
+		if currentIOC > bestIOC {
+			bestIOC = currentIOC
+			bestPlugboard = currentBestPlugboard
+		}
+	}
+
+	// fmt.Printf("\nbestIOC: %v", bestIOC)
+	// fmt.Printf("\nbestPlugboard: %v", bestPlugboard)
+
+	return bestPlugboard
+}
+
+// runEnigma configures and run the Enigma with the given settings
+// and returns the output
+func runEnigma(input string, config enigma) string {
+	newConfig := make([]enigma_api.RotorConfig, len(config.rotors))
+	for index, rotor := range config.rotors {
+		ring := config.rings[index]
+		start := config.positions[index][0]
+		newConfig[index] = enigma_api.RotorConfig{ID: rotor, Start: start, Ring: ring}
+	}
+
+	formattedPlugboard := formatPlugboard(config.plugboard)
+	// fmt.Printf("\nRawplugboard: %v", defaultPlugboard)
+	// fmt.Printf("\nRawplugboard: %v", plugboard)
+	// fmt.Printf("\nFormatted plugboard: %v", formattedPlugboard)
+
+	e := enigma_api.NewEnigma(newConfig, config.reflector, formattedPlugboard)
+	output := e.EncodeString(input)
+
+	return output
+}
+
+// runEnigmaWithPlugboard is a wrapper for `runEnigma()` and takes in
+// plugboard configuration instead of `enigma` struct
+func runEnigmaWithPlugboard(input, plugboard string) string {
+	var config enigma
+	copyStruct(&config, &currentConfig)
+
+	config.plugboard = plugboard
+
+	return runEnigma(input, config)
+}
+
+// formatPlugboard converts to raw plugboard string into the format
+// expected by `github.com/emedvedev/enigma` (string array)
+func formatPlugboard(rawPlugboard string) []string {
+	formattedPlugboard := make([]string, 0)
+
+	for i, letter := range rawPlugboard {
+		if string(rune(i+65)) < string(letter) {
+			formattedPlugboard = append(formattedPlugboard, string(rune(i+65))+string(letter))
+		}
+	}
+
+	return formattedPlugboard
+}
+
+// swap swaps two characters in a given string and returns the new string
+func swap(a, b rune, plugboard string) string {
+	// fmt.Printf("\n Swap %v and %v", string(a), string(b))
+
+	for i, letter := range plugboard {
+		if letter == a {
+			plugboard = plugboard[0:i] + string(b) + plugboard[i+1:]
+		} else if letter == b {
+			plugboard = plugboard[0:i] + string(a) + plugboard[i+1:]
+		}
+	}
+
+	return plugboard
+}
+
+// copyStruct makes a deep copy of `Enigma` struct into another
+// struct
+func copyStruct(dst *enigma, src *enigma) {
+	dst.reflector = src.reflector
+
+	dst.rings = make([]int, len(src.rings))
+	copy(dst.rings, src.rings)
+
+	dst.positions = make([]string, len(src.positions))
+	copy(dst.positions, src.positions)
+
+	dst.rotors = make([]string, len(src.rotors))
+	copy(dst.rotors, src.rotors)
+
+	dst.plugboard = src.plugboard
+}
+
+// calculateIOC calculates the `Index of Coincidence` for a given text
+func calculateIOC(text string) float64 {
+	ioc := float64(0)
+
+	for ascii := 65; ascii < 91; ascii++ {
+		fi := strings.Count(text, string(rune(ascii)))
+		ioc += float64(fi * (fi - 1))
+	}
+
+	n := float64(len(text))
+	ioc = float64(ioc / (n * (n - 1)))
+
+	return ioc
+}
+
 // calculateTrigram calculates the trigram score for a given text
 func calculateTrigram(text string) float64 {
 	score := float64(0)
@@ -254,4 +228,25 @@ func calculateTrigram(text string) float64 {
 	}
 
 	return score
+}
+
+// printConfig pretty prints the configuration of the Enigma
+func printConfig(config enigma) {
+	fmt.Printf("\n\n Enigma configuration:")
+
+	fmt.Printf("\n\n\t Rotor configuration:\n\t\t")
+	for _, rotor := range config.rotors {
+		fmt.Printf("%v ", rotor)
+	}
+
+	fmt.Printf("\n\n\t Rotor position configuration:\n\t\t")
+	for _, position := range config.positions {
+		fmt.Printf("%v ", position)
+	}
+
+	fmt.Printf("\n\n\t Plugboard configuration:\n")
+	fmt.Printf("\t\t%v", config.plugboard)
+	fmt.Printf("\n\t\t%v", formatPlugboard(config.plugboard))
+
+	fmt.Printf("\n\n\n")
 }
